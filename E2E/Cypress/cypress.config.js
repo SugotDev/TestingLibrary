8255a6d78ca3aa670a8a98c470f13982fd06b47e
SugotDev <m.p.slugocki@gmail.com>
2025-09-28 17:44:55 +0200

    Update cypress config

--- E2E/Cypress/cypress.config.js
+++ E2E/Cypress/cypress.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "https://www.saucedemo.com/",
+    specPattern: "cypress/e2e/saucedemo/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
+    video: false,
+    screenshotsFolder: "cypress/screenshots",
+    videosFolder: "cypress/videos",
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

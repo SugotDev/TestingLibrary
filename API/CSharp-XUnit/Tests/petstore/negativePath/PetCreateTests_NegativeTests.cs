d51528502d95140964e761f08652879a5bfd8170
SugotDev <m.p.slugocki@gmail.com>
2025-10-12 09:20:27 +0200

    Create xunit test cases

---
+++ API/CSharp-XUnit/Tests/petstore/negativePath/PetCreateTests_NegativeTests.cs
@@ -0,0 +1,55 @@
+﻿using ApiTests.Models;
+using FluentAssertions;
+using System.Net;
+
+namespace CSharp_XUnit.Tests.petstore.positivePath
+{
+    public class PetCreateTests_NegativeTests : BaseApiTest
+    {
+        public PetCreateTests_NegativeTests(ApiFixture fixture) : base(fixture) { }
+
+        [Fact]
+        public async Task CreatePetWithMissingName()
+        {
+            // Arrage
+            var pet = _pets["nemo"];
+            pet.Name = " ";
+
+            // Act
+            var postResponse = await _api.CreatePetAsync(pet);
+
+            // Assert
+            postResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            postResponse.Content.Should().Contain("Pet name is required");
+        }
+
+        [Fact]
+        public async Task CreateEmptyPet()
+        {
+            //Arrange
+            var pet = new Pet();
+
+            //Act
+            var postResponse = await _api.CreatePetAsync(pet);
+
+            //Assert
+            postResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            postResponse.Content.Should().Contain("Pet id is required");
+
+        }
+
+        [Fact]
+        public async Task CreateExistingPet()
+        {
+            //Arrange
+            var pet = _pets["rex"];
+
+            //Act
+            var postResponse = await _api.CreatePetAsync(pet);
+
+            //Assert
+            postResponse.StatusCode.Should().Be(HttpStatusCode.Conflict);
+            postResponse.Content.Should().Contain("already exists");
+        }
+    }
+}
---
+++ API/CSharp-XUnit/Tests/petstore/negativePath/PetDeleteTests_NegativeTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using System.Net;
+
+namespace CSharp_XUnit.Tests.petstore.positivePath
+{
+    public class PetDeleteTests_NegativeTests : BaseApiTest
+    {
+        public PetDeleteTests_NegativeTests(ApiFixture fixture) : base(fixture) { }
+
+        [Fact]
+        public async Task DeleteNonExistingPet()
+        {
+            //Arrange
+            var petId = "123";
+
+            //Act
+            var deleteResponse = await _api.DeletePetAsync(petId);
+
+            //Assert
+            deleteResponse.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            deleteResponse.StatusDescription.Should().Contain("Not Found");
+        }
+
+    }
+}

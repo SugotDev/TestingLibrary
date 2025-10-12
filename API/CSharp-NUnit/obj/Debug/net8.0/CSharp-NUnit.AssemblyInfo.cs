bcedc3cc9db7f54aaa3e041b55688bffba587da6
SugotDev <m.p.slugocki@gmail.com>
2025-10-12 09:24:36 +0200

    Update

--- API/CSharp-NUnit/obj/Debug/net8.0/CSharp-NUnit.AssemblyInfo.cs
+++ API/CSharp-NUnit/obj/Debug/net8.0/CSharp-NUnit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp-NUnit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95a85e3d5a1c4d75360e65226c553ee22e29a9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de338d8593d17420a476d7e38a7396d25073adc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp-NUnit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp-NUnit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
